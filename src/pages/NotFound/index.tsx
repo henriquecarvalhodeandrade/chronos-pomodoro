44923174e6d20b508b57b4ea7abab582a123f793
Henrique Andrade <rique9906@gmail.com>
2026-03-11 13:51:15 -0300

    refactor: agora existe um template principal para as páginas, no app retornamos apenas as páginas e não mais cada componente.

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../../components/Container";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1>Página não encontrada!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati neque doloremque similique placeat excepturi incidunt laudantium nisi ipsum ipsam at, maiores molestias modi possimus inventore repellendus voluptatem harum dignissimos non.</p>
+      </Container>
+    </MainTemplate>
+  );
+}
